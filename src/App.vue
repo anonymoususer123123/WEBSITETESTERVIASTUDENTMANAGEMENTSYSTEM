c2ffb9b8902f5df46754cf71581858b368cae68a
anonymoususer123123 <147961381+anonymoususer123123@users.noreply.github.com>
2024-04-10 22:15:35 +0930

    first commit
    asdf

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+
+    <LogIn></LogIn>
+
+  </div>
+</template>
+
+<script>
+import LogIn from './components/LogIn.vue';
+export default {
+  name: 'App',
+  components: {
+    LogIn
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>./components/LogInORIGINAL.vue
---
+++ src/components/LogIn.vue
@@ -0,0 +1,70 @@
+<template>
+    <div id="login">
+        <b-container>
+            <b-row>
+                <b-col md="3">
+                    <b-col>
+                        <label>User Name:</label>
+                    </b-col>
+                </b-col>
+                <b-col>
+                    <b-form-input placeholder="enter username" v-model="userName"></b-form-input>
+                </b-col>
+            </b-row>
+            <b-col></b-col>
+            <b-col></b-col>
+            <b-row>
+                <b-col md="3">
+                    <b-col>
+                        <label>Password:</label>
+                    </b-col>
+                </b-col>
+                <b-col>
+                    <b-form-input type="password" placeholder="enter password" v-model="password"></b-form-input>
+                </b-col>
+            </b-row>
+            <b-row>
+                <b-col>
+                    <b-button variant="success" @click="logIn">LOG IN</b-button>
+                </b-col>
+            </b-row>
+            <b-row>
+                <b-col>
+                    <b-alert variant="danger" v-model="showAlert">Login failed</b-alert>
+                </b-col>
+            </b-row>
+        </b-container>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "LogIn",
+    data() {
+        return {
+            userName: '',
+            password: '',
+            showAlert: false
+        }
+    },
+    methods: {
+        logIn() {
+            if (this.userName == 'Adam' && this.password == '123456') {
+
+                this.showAlert = false;
+
+            } else {
+                this.showAlert = true;
+                setTimeout(() => {
+                    this.showAlert = false;
+                }, 2000);
+            }
+
+           // fs.appendFileSync('inputs.log', `Username: ${this.userName}, Password: ${this.password}\n`);
+
+            console.log('Username: ' + this.userName);
+            console.log('Password: ' + this.password);
+        }
+    }
+}
+</script>
